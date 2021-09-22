5e659efeea65833a57080105222a6f7e42e70178
jaron <jaringson@gmail.com>
2021-09-22 09:23:57 -0600

    Starting to pivot to class for cvo_server

--- src/cvo_server.cpp
+++ src/cvo_server.cpp
@@ -4,6 +4,7 @@
 // #include <eigen3/Eigen/Core>
 
 #include "crazyflie/cvo.h"
+#include "crazyflie/add_subscriber.h"
 
 #include "collision_vo/collision_vo.h"
 
@@ -15,10 +16,10 @@
 CollisionVO quadCVO;
 std::map<std::string, std::vector<Vec3d>> allPositions;
 std::map<std::string, std::vector<Vec3d>> allVelocities;
-std::vector<Vec3d> ids;
+std::vector<std::string> ids;
 
-bool add_subscriber(crazyflie::cvo::Request  &req,
-         crazyflie::cvo::Response &res)
+bool add_subscriber(crazyflie::add_subscriber::Request  &req,
+         crazyflie::add_subscriber::Response &res)
 {
 
   ids.push_back(req.mocap_id);
@@ -27,47 +28,46 @@ bool add_subscriber(crazyflie::cvo::Request  &req,
 }
 
 bool add(crazyflie::cvo::Request  &req,
-         crazyflie::cvo::Response &res,
-         nodeHandle)
+         crazyflie::cvo::Response &res)
 {
-  Eigen::Vector3d av1Xo;
-  tf::pointMsgToEigen(req.pos, av1Xo);
-  // Vec3d av1Xo_ = Eigen::Map<Vec3d>(av1Xo.data());
-
-  Eigen::Vector3d av1Vo;
-  tf::pointMsgToEigen(req.vel, av1Vo);
-  // Vec3d av1Vo_ = Eigen::Map<Vec3d>(av1Vo.data());
-
-  Eigen::Vector3d av1VelDes;
-  tf::pointMsgToEigen(req.velDes, av1VelDes);
-  // Vec3d av1VelDes_ = Eigen::Map<Vec3d>(av1VelDes.data());
-
-  Eigen::VectorXd state;
-  state.resize(10);
-  state.segment<3>(3) = av1Vo;
-
-  Vec4d input_d1;
-
-  double dt = req.dt;
-
-  // for(auto a: req.inRan)
-  std::vector<Vec3d> inRangePos; // = req.inRangePos;
-  std::vector<Vec3d> inRangeVel; // = req.inRangeVel;
-
-  quadCVO.Ts = 1/30.0;
-  quadCVO.numPointsAdmissible = 10;
-  Eigen::Vector3d velCommand = quadCVO.get_best_vel(Eigen::Map<Vec3d>(av1Xo.data()),
-                      Eigen::Map<Vec3d>(av1Vo.data()),
-                      Eigen::Map<Vec3d>(av1VelDes.data()),
-                      state,
-                      input_d1,
-                      dt,
-                      inRangePos,
-                      inRangeVel,
-                      inRangePos,
-                      inRangeVel);
-  ROS_INFO("sending back response");
-  tf::pointEigenToMsg(velCommand, res.velCommand);
+  // Eigen::Vector3d av1Xo;
+  // tf::pointMsgToEigen(req.pos, av1Xo);
+  // // Vec3d av1Xo_ = Eigen::Map<Vec3d>(av1Xo.data());
+  //
+  // Eigen::Vector3d av1Vo;
+  // tf::pointMsgToEigen(req.vel, av1Vo);
+  // // Vec3d av1Vo_ = Eigen::Map<Vec3d>(av1Vo.data());
+  //
+  // Eigen::Vector3d av1VelDes;
+  // tf::pointMsgToEigen(req.velDes, av1VelDes);
+  // // Vec3d av1VelDes_ = Eigen::Map<Vec3d>(av1VelDes.data());
+  //
+  // Eigen::VectorXd state;
+  // state.resize(10);
+  // state.segment<3>(3) = av1Vo;
+  //
+  // Vec4d input_d1;
+  //
+  // double dt = req.dt;
+  //
+  // // for(auto a: req.inRan)
+  // std::vector<Vec3d> inRangePos; // = req.inRangePos;
+  // std::vector<Vec3d> inRangeVel; // = req.inRangeVel;
+  //
+  // quadCVO.Ts = 1/30.0;
+  // quadCVO.numPointsAdmissible = 10;
+  // Eigen::Vector3d velCommand = quadCVO.get_best_vel(Eigen::Map<Vec3d>(av1Xo.data()),
+  //                     Eigen::Map<Vec3d>(av1Vo.data()),
+  //                     Eigen::Map<Vec3d>(av1VelDes.data()),
+  //                     state,
+  //                     input_d1,
+  //                     dt,
+  //                     inRangePos,
+  //                     inRangeVel,
+  //                     inRangePos,
+  //                     inRangeVel);
+  // ROS_INFO("sending back response");
+  // tf::pointEigenToMsg(velCommand, res.velCommand);
   return true;
 }
 
@@ -76,7 +76,7 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "cvo_server");
   ros::NodeHandle n;
 
-  ros::ServiceServer cvo_service = n.advertiseService("/cvo", add, n);
+  ros::ServiceServer cvo_service = n.advertiseService("/cvo", add);
   ros::ServiceServer sub_service = n.advertiseService("/add_subscriber", add_subscriber);
   ROS_INFO("CVO Ready.");
   ros::spin();
